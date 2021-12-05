44950a90e7c42a75c4edf13d029f0008a8d8f763
yonghduan <yongh_duan@163.com>
2021-12-05 23:01:08 +0800

    modified startup.m

---
+++ startup.m
@@ -0,0 +1,90 @@
+function startup()
+%STARTUP Add the directory containing this script to MATLAB path automatically.
+%
+% This script will perform the installation in the first-time use.
+% After that, every time MATLAB boots up, the directory containing this script
+% is added to path automatically.
+    
+    path2ThisScript = which(mfilename);
+    toolSetPath = fileparts(path2ThisScript);
+    originPath = pwd;
+    isInFatherDirectory = false;
+   
+    % disp(['Current startup.m file path: ' path2ThisScript]);
+    cd (toolSetPath);
+    cd ../;
+    if exist('SDR','dir') && exist('@RXSBundle','dir') && exist('@RTRXSBundle','dir')
+        toolSetPath = pwd;
+        isInFatherDirectory = true;
+    end
+    
+    if exist('PicoScenes-MATLAB-Toolbox-Core','dir')
+        cd PicoScenes-MATLAB-Toolbox-Core/;
+    elseif exist('MATLAB','dir')
+        cd MATLAB/;
+    end
+
+    if strcmp(userpath, toolSetPath) == true
+        isInUserPath = true;
+    else
+        isInUserPath = false;
+    end
+    
+    if isInUserPath == false
+        if isempty(userpath) == true % Create /Documents/MATLAB directory if not existent.
+            if ispc == true
+                dirname = [getenv('USERPROFILE') '/Documents/MATLAB'];
+                if ~exist(dirname, 'dir')
+                    mkdir(dirname);
+                end
+            else
+                dirname = [getenv('HOME') '/Documents/MATLAB'];
+                if ~exist(dirname, 'dir')
+                    mkdir(dirname);
+                end
+            end
+            userpath('reset');
+        end
+        
+        copyfile(path2ThisScript, userpath);
+        writePath(toolSetPath,isInFatherDirectory);
+        toolSetPath = readToolSetPath();
+        addpath(genpath(toolSetPath));
+    end
+    
+    if isInUserPath == true
+        [toolSetPath,isInFatherDirectory] = readToolSetPath();
+        disp(['PicoScenes MATLAB Toolbox [' toolSetPath '] added to the MATLAB search path.']);
+        addpath(genpath(toolSetPath));
+
+        if(strcmp(isInFatherDirectory,'true'))
+            startupScriptPathInToolSet = [readToolSetPath filesep 'PicoScenes-MATLAB-Toolbox-Core\startup.m'];
+        else
+            startupScriptPathInToolSet = [readToolSetPath filesep 'startup.m'];
+        end
+        copyfile(startupScriptPathInToolSet, userpath);
+       
+    end
+    cd (originPath);
+    
+end
+
+function writePath(toolSetPath,isInFatherDirectory)
+    targetPath = [userpath filesep 'PicoScenes-MATLAB-Toolbox-Path.txt'];
+    fid = fopen(targetPath,'wt');
+    fprintf(fid, '%s\n', toolSetPath);
+    if(isInFatherDirectory)
+        fprintf(fid,'%s','true');
+    else
+        fprintf(fid,'%s','false');
+    end
+    fclose(fid);
+end
+
+function [toolSetPath,isInFatherDirectory] = readToolSetPath()
+    targetPath = [userpath filesep 'PicoScenes-MATLAB-Toolbox-Path.txt'];
+    fid = fopen(targetPath,'r');
+    toolSetPath = fgetl(fid);
+    isInFatherDirectory = fgetl(fid);
+    fclose(fid);
+end
