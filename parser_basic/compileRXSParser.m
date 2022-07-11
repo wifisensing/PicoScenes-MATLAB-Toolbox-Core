9b43db1263ff3f12bc68b511ce607d912755b901
Zhiping Jiang <zpj@xidian.edu.cn>
2022-07-11 18:45:49 +0800

    Upgrade compileRXSParser.m

--- parser_basic/compileRXSParser.m
+++ parser_basic/compileRXSParser.m
@@ -1,13 +1,23 @@
-function compileRXSParser
+function compileRXSParser(skipExtraParam)
+
+if nargin == 0
+    skipExtraParam = false;
+end
 
 disp('Compiling RXSParser.cxx, the MATLAB parser for PicoScenes .csi file ...');
 currentDir = pwd;
 cd(fileparts(which(mfilename))); % change the MATLAB working directory to the folder of this file.
+
+extraParam = '';
+if ~skipExtraParam && exist([pwd filesep '..' filesep '..' filesep 'utils'], 'dir')
+    extraParam = fileread([pwd filesep '..' filesep '..' filesep 'utils' filesep 'compileRXSParserExtraParam']);
+end
+
 try
-    mex -silent -DBUILD_WITH_MEX CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3" RXSParser.cxx ../rxs_parsing_core/*.cxx ../rxs_parsing_core/preprocess/generated/*.cpp
+    eval(['mex -silent -DBUILD_WITH_MEX CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3" RXSParser.cxx ../rxs_parsing_core/*.cxx ../rxs_parsing_core/preprocess/generated/*.cpp ' extraParam])
 catch 
     warning('Exception caught! Use verbose mode to build again.');
-    mex -v -DBUILD_WITH_MEX CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3" RXSParser.cxx ../rxs_parsing_core/*.cxx ../rxs_parsing_core/preprocess/generated/*.cpp
+    eval(['mex -v -DBUILD_WITH_MEX CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3" RXSParser.cxx ../rxs_parsing_core/*.cxx ../rxs_parsing_core/preprocess/generated/*.cpp ' extraParam])
 end
 
 cd(currentDir);
