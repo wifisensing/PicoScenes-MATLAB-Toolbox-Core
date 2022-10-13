6d5043aa147771cf6cbed3c5f87007d1ea55a870
Zhiping Jiang <zpj@xidian.edu.cn>
2022-10-13 19:20:22 +0800

    add private ProtocolHandler mechanism

--- parser_basic/opencsi.m
+++ parser_basic/opencsi.m
@@ -40,9 +40,17 @@ assignin('base', bundleValidName, bundle);
 assignin('base', 'latest', bundle);
 disp(bundleValidName);
 
-if exist('RTTProtocolCombiner', 'file')
-    RTTProtocolCombiner(bundleValidName);
+currentDir = pwd;
+cd(fileparts(which(mfilename))); % change the MATLAB working directory to the folder of this file.
+path2ProtocolHandler = [pwd filesep '..' filesep '..' filesep 'utils' filesep 'ProtocolHandler'];
+if exist(path2ProtocolHandler, 'dir')
+    protocolList = dir([path2ProtocolHandler filesep '*.m'])';
+    for file = protocolList
+        [~, mName] = fileparts(file.name);
+        evalin('base', [mName '("' bundleValidName '")']);
+    end
 end
+cd(currentDir);
 
 if nargout == 0
     bundle = [];
