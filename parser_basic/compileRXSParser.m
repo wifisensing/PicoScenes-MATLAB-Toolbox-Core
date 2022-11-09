8b1efd238a293a230d7bef187fa4456835a23e83
labcomputerWindows <yongh_duan@163.com>
2022-11-09 15:25:06 +0800

    rx parser

--- parser_basic/compileRXSParser.m
+++ parser_basic/compileRXSParser.m
@@ -6,9 +6,17 @@ end
 
 disp('Compiling RXSParser.cxx, the MATLAB parser for PicoScenes .csi file ...');
 currentDir = pwd;
-cd(fileparts(which(mfilename))); % change the MATLAB working directory to the folder of this file.
 
-extraParam = '';
+cd(fileparts(which(mfilename))); % change the MATLAB working directory to the folder of this file.
+cd ../rxs_parsing_core;
+direcory = [pwd filesep '*.cxx'];
+allFiles = dir(direcory);
+[cppNum,~] = size(allFiles);
+if cppNum <= 0
+    error(['There are no files in directory ' pwd ',please go to <a href = "https://gitlab.com/wifisensing/rxs_parsing_core ">https://gitlab.com/wifisensing/rxs_parsing_core </a>to download files']);
+end
+cd(fileparts(which(mfilename))); 
+
 if ~skipExtraParam && exist([pwd filesep '..' filesep '..' filesep 'utils'], 'dir')
     extraParam = fileread([pwd filesep '..' filesep '..' filesep 'utils' filesep 'compileRXSParserExtraParam']);
 end
