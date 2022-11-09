1dda385b21bb8cc85da9580f92a6768ad72abfb3
Zhiping Jiang <zpj@xidian.edu.cn>
2022-11-09 18:15:56 +0800

    add parsing-core checking before compile RXSParser

--- parser_basic/compileRXSParser.m
+++ parser_basic/compileRXSParser.m
@@ -1,35 +1,35 @@
 function compileRXSParser(skipExtraParam)
-
-if nargin == 0
-    skipExtraParam = false;
+    if nargin == 0
+        skipExtraParam = false;
+    end
+    
+    checkRXSParsingCoreExists;    
+    disp('Compiling RXSParser.cxx, the MATLAB parser for PicoScenes .csi file ...');
+    currentDir = pwd;
+    cd(fileparts(which(mfilename))); 
+    
+    extraParam = '';
+    if ~skipExtraParam && exist([pwd filesep '..' filesep '..' filesep 'utils'], 'dir')
+        extraParam = fileread([pwd filesep '..' filesep '..' filesep 'utils' filesep 'compileRXSParserExtraParam']);
+    end
+    
+    try
+        eval(['mex -silent -DBUILD_WITH_MEX CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3" -I../rxs_parsing_core RXSParser.cxx ../rxs_parsing_core/*.cxx ../rxs_parsing_core/preprocess/generated/*.cpp ' extraParam]);
+        disp('Compilation done!');
+    catch 
+        warning('Exception caught! Use verbose mode to build again.');
+        eval(['mex -v -DBUILD_WITH_MEX CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3" -I../rxs_parsing_core RXSParser.cxx ../rxs_parsing_core/*.cxx ../rxs_parsing_core/preprocess/generated/*.cpp ' extraParam])
+    end
+    
+    cd(currentDir);
 end
 
-disp('Compiling RXSParser.cxx, the MATLAB parser for PicoScenes .csi file ...');
-currentDir = pwd;
-
-cd(fileparts(which(mfilename))); % change the MATLAB working directory to the folder of this file.
-cd ../rxs_parsing_core;
-parseDir = pwd;
-direcory = [pwd filesep '*.cxx'];
-allFiles = dir(direcory);
-[cppNum,~] = size(allFiles);
-if cppNum <= 0
-    cd (currentDir);
-    error(['There are no files in directory ' parseDir ',please go to <a href = "https://gitlab.com/wifisensing/rxs_parsing_core ">https://gitlab.com/wifisensing/rxs_parsing_core </a>to download files']);
+function checkRXSParsingCoreExists
+    cd([fileparts(which(mfilename)) filesep '..' filesep 'rxs_parsing_core']);
+    parserDir = pwd;
+    allFiles = dir([pwd filesep '*.cxx']);
+    if isempty(allFiles)
+        cd (currentDir);
+        error(['The [' parserDir '] directory is empty! That means you have NOT clone the PicoScenes MATALB Toolbox RECURSIVELY. Please refer to page <a href = "https://gitlab.com/wifisensing/PicoScenes-MATLAB-Toolbox-Core">PicoScenes MATLAB Toolbox Core</a> on how to clone the toolbox.']);
+    end
 end
-cd(fileparts(which(mfilename))); 
-
-extraParam = '';
-if ~skipExtraParam && exist([pwd filesep '..' filesep '..' filesep 'utils'], 'dir')
-    extraParam = fileread([pwd filesep '..' filesep '..' filesep 'utils' filesep 'compileRXSParserExtraParam']);
-end
-
-try
-    eval(['mex -silent -DBUILD_WITH_MEX CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3" -I../rxs_parsing_core RXSParser.cxx ../rxs_parsing_core/*.cxx ../rxs_parsing_core/preprocess/generated/*.cpp ' extraParam]);
-    disp('Compilation done!');
-catch 
-    warning('Exception caught! Use verbose mode to build again.');
-    eval(['mex -v -DBUILD_WITH_MEX CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3" -I../rxs_parsing_core RXSParser.cxx ../rxs_parsing_core/*.cxx ../rxs_parsing_core/preprocess/generated/*.cpp ' extraParam])
-end
-
-cd(currentDir);
