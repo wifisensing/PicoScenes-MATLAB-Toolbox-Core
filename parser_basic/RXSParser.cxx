9978edb2ac4ff225cdf1722efddc5b9fdc53680d
Zhiping Jiang <zpj@xidian.edu.cn>
2023-05-14 19:20:51 +0800

    update parsing_core

--- parser_basic/RXSParser.cxx
+++ parser_basic/RXSParser.cxx
@@ -535,6 +535,7 @@ mxArray *convertSDRExtra2MxArray(const SDRExtra &sdrExtra) {
     mxSetFieldByNumber(sdrExtraArray, 0, mxAddField(sdrExtraArray, "LastTxTime"), createScalarMxArray(sdrExtra.lastTxTime));
     mxSetFieldByNumber(sdrExtraArray, 0, mxAddField(sdrExtraArray, "SignalInputTime"), createScalarMxArray(sdrExtra.signalInputSystemTime));
     mxSetFieldByNumber(sdrExtraArray, 0, mxAddField(sdrExtraArray, "SignalDecodingTime"), createScalarMxArray(sdrExtra.signalDecodeSystemTime));
+    mxSetFieldByNumber(sdrExtraArray, 0, mxAddField(sdrExtraArray, "SIGEVM"), createScalarMxArray(sdrExtra.sigEVM));
     mxSetFieldByNumber(sdrExtraArray, 0, mxAddField(sdrExtraArray, "DecodingDelay"), createScalarMxArray(sdrExtra.signalDecodeSystemTime - sdrExtra.signalInputSystemTime));
 
     return sdrExtraArray;
