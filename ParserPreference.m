7633623bac162322d0db59c4433c30ccde7a1a48
Zhiping Jiang <zpj@xidian.edu.cn>
2024-03-15 15:37:58 +0800

    update

--- ParserPreference.m
+++ ParserPreference.m
@@ -3,8 +3,7 @@ classdef ParserPreference
     methods (Static)
         function preference = getPreference
 
-            preference.skipBasebandSignals = true;  % If true, remove the parsed baseband signals, which saves a lot of memory
-            preference.forceBundledParsing = false; % If true, force all Rx frames to have the same CSI size/parameters to activate bundled parsing.
+            preference.skipBasebandSignals = false;  % If true, remove the parsed baseband signals, which saves a lot of memory
 
         end
 
