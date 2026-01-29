9a915efc65bd38b76548a25534c4dbedc54621cc
zzz <zxubiao364@gmail.com>
2026-01-29 22:18:28 +0800

    Revert "updata"
    This reverts commit 73da505de1dac7c6407a3e57dde2807c95443475.

--- parser_basic/compileRXSParser.m
+++ parser_basic/compileRXSParser.m
@@ -7,7 +7,6 @@ function compileRXSParser(skipExtraParam)
     checkRXSParsingCoreExists(currentDir);    
     disp('Compiling the MATLAB parser for PicoScenes .csi file ...');
     
-    % 切换到脚本所在目录
     cd(fileparts(which(mfilename))); 
     
     extraParam = '';
@@ -15,30 +14,12 @@ function compileRXSParser(skipExtraParam)
         extraParam = fileread([pwd filesep '..' filesep '..' filesep 'utils' filesep 'compileRXSParserExtraParam']);
     end
     
-    % --- 核心修改部分：根据 OS 构建参数 ---
-    commonFlags = '-DBUILD_WITH_MEX -I../rxs_parsing_core';
-    sources = 'RXSParser.cxx ../rxs_parsing_core/*.cxx ../rxs_parsing_core/preprocess/generated/*.cpp';
-    
-    if ispc
-        % Windows 专用宏定义
-        osSpecificFlags = '-DRXSPARSINGCORE_STATIC -DRXS_PARSING_CORE_STATIC -DRXS_PARSING_CORE_EXPORTS';
-        % MSVC 默认支持 C++20 往往需要 /std:c++20，但 mex 内部会自动映射
-        cxxFlags = 'CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3"';
-    else
-        % Ubuntu/macOS 
-        osSpecificFlags = '';
-        cxxFlags = 'CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3"';
-    end
-
-    cmdBase = sprintf('mex %s %s %s %s %s', commonFlags, osSpecificFlags, cxxFlags, sources, extraParam);
-
     try
-        fprintf('Running: %s\n', ['mex -silent ' cmdBase]);
-        eval(['mex -silent ' cmdBase]);
+        eval(['mex -silent -DBUILD_WITH_MEX CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3" -I../rxs_parsing_core RXSParser.cxx ../rxs_parsing_core/*.cxx ../rxs_parsing_core/preprocess/generated/*.cpp ' extraParam]);
         disp('Compilation done!');
     catch 
         warning('Exception caught! Use verbose mode to build again.');
-        eval(['mex -v ' cmdBase]);
+        eval(['mex -v -DBUILD_WITH_MEX CXXFLAGS="$CXXFLAGS -std=c++2a -Wno-attributes -O3" -I../rxs_parsing_core RXSParser.cxx ../rxs_parsing_core/*.cxx ../rxs_parsing_core/preprocess/generated/*.cpp ' extraParam])
     end
     
     cd(currentDir);
@@ -50,6 +31,6 @@ function checkRXSParsingCoreExists(currentDir)
     allFiles = dir([pwd filesep '*.cxx']);
     if isempty(allFiles)
         cd (currentDir);
-        error(['The [' parserDir '] directory is empty! That means you have NOT cloned the PicoScenes MATLAB Toolbox RECURSIVELY. Please refer to page <a href = "https://github.com/wifisensing/PicoScenes-MATLAB-Toolbox-Core">PicoScenes MATLAB Toolbox Core</a> on how to clone the toolbox correctly.']);
+        error(['The [' parserDir '] directory is empty! That means you have NOT cloned the PicoScenes MATALB Toolbox RECURSIVELY. Please refer to page <a href = "https://github.com/wifisensing/PicoScenes-MATLAB-Toolbox-Core">PicoScenes MATLAB Toolbox Core</a> on how to clone the toolbox correctly.']);
     end
 end
